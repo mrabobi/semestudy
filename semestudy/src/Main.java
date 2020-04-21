fed5293dfc49ba839d6b405919568d6b83a17a7d
BOBIRE <mraboby@gmail.com>
2020-04-21 23:46:17 +0300

    Upload java solution

---
+++ semestudy/src/Main.java
@@ -0,0 +1,8 @@
+import GUI.connectionForm;
+
+public class Main {
+    public static void main(String[] args){
+        connectionForm conn = new connectionForm();
+        conn.setVisible(true);
+    }
+}
